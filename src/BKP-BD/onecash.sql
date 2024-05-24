7a2cc8d8794e89f4b256f8a41d2c4b4d81703979
Gideone Lacerda Costa <89115386+GidLC@users.noreply.github.com>
2024-05-23 23:18:26 -0300

    bkp bd 23-05-2024

--- src/BKP-BD/onecash.sql
+++ src/BKP-BD/onecash.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18/05/2024 às 15:03
+-- Tempo de geração: 24/05/2024 às 04:17
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.0.30
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,8 +30,26 @@ SET time_zone = "+00:00";
 CREATE TABLE `aporte_objetivo` (
   `id` int(11) NOT NULL,
   `valor` float NOT NULL,
-  `objetivo` int(11) NOT NULL
+  `objetivo` int(11) NOT NULL,
+  `casal` varchar(20) NOT NULL,
+  `data` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `aporte_objetivo`
+--
+
+INSERT INTO `aporte_objetivo` (`id`, `valor`, `objetivo`, `casal`, `data`) VALUES
+(3, 67.98, 4, '6d8b97', '2024-05-23'),
+(4, 159.56, 4, '6d8b97', '2024-05-23'),
+(5, 350, 4, '6d8b97', '2024-05-23'),
+(7, 200, 8, '6d8b97', '2024-05-23'),
+(9, 2000, 4, '6d8b97', '2024-05-23'),
+(10, 2.5, 15, '6d8b97', '2024-05-23'),
+(11, 250, 8, '6d8b97', '2024-05-23'),
+(14, 950, 8, '6d8b97', '2024-05-23'),
+(15, 8000, 6, '6d8b97', '2024-05-23'),
+(16, 2000, 6, '6d8b97', '2024-05-23');
 
 -- --------------------------------------------------------
 
@@ -54,13 +72,15 @@ CREATE TABLE `banco` (
 --
 
 INSERT INTO `banco` (`id`, `nome`, `tipo`, `saldo_inicial`, `casal`, `usuario`, `arquivo`) VALUES
-(36, 'Carteira', 0, 1501.01, '6d8b97', 158, 0),
-(37, 'Nossa Conta', 1, -17850, '6d8b97', 158, 0),
+(36, 'Carteira', 0, 1541.01, '6d8b97', 158, 0),
+(37, 'Nossa Conta', 1, -17850, '6d8b97', 158, 1),
 (38, 'Carteira', 0, 0, '6d8b97', 159, 0),
 (39, 'Bradesco', 1, 2500, '6d8b97', NULL, 0),
 (44, 'NuBank', 0, 4909.53, '6d8b97', 158, 0),
 (51, 'Itaú', 0, 7275.11, '6d8b97', 158, 0),
-(52, 'Bradesco', 0, 11.96, '6d8b97', 158, 0);
+(52, 'Bradesco', 0, 11.96, '6d8b97', 158, 1),
+(53, 'Carteira', 0, 0, '4aa97f', 180, 0),
+(54, 'Nossa Conta', 1, 0, '4aa97f', 180, 0);
 
 -- --------------------------------------------------------
 
@@ -80,7 +100,8 @@ CREATE TABLE `casal` (
 --
 
 INSERT INTO `casal` (`id`, `cod_casal`, `usuario_princ`, `usuario_sec`) VALUES
-(40, '6d8b97', 158, 159);
+(40, '6d8b97', 158, 159),
+(42, '4aa97f', 180, NULL);
 
 -- --------------------------------------------------------
 
@@ -103,19 +124,33 @@ CREATE TABLE `categoria_tr` (
 --
 
 INSERT INTO `categoria_tr` (`id`, `nome`, `tipo`, `cor`, `icone`, `casal`, `cat_sistema`) VALUES
-(111, 'Moradia', 0, 3, 27, '6d8b97', 0),
+(111, 'Moradia', 0, 16, 27, '6d8b97', 0),
 (113, 'Saúde', 0, 11, 29, '6d8b97', 0),
 (116, 'Roupas e Acessórios', 0, 8, 33, '6d8b97', 0),
 (117, 'Água/Luz/Internet', 0, 9, 24, '6d8b97', 0),
-(119, 'Salário', 1, 11, 38, '6d8b97', 0),
+(119, 'Salário', 1, 11, 5, '6d8b97', 0),
 (122, 'Vales', 1, 14, 31, '6d8b97', 0),
-(123, 'Vendas ', 1, 3, 37, '6d8b97', 0),
+(123, 'Vendas ', 1, 3, 38, '6d8b97', 0),
 (141, '*Ajuste*', 0, 2, 36, '6d8b97', 1),
 (142, '*Ajuste*', 1, 3, 37, '6d8b97', 1),
 (144, 'Despesas Diversas ', 0, 16, 36, '6d8b97', 0),
-(147, 'Presente ', 1, 2, 26, '6d8b97', 0),
 (148, 'Lazer', 0, 12, 25, '6d8b97', 0),
-(150, 'Teste', 1, 16, 11, '6d8b97', 0);
+(153, 'Alimentação', 0, 2, 21, '4aa97f', NULL),
+(154, 'Moradia', 0, 3, 27, '4aa97f', NULL),
+(155, 'Transporte', 0, 4, 16, '4aa97f', NULL),
+(156, 'Saúde', 0, 5, 29, '4aa97f', NULL),
+(157, 'Educação', 0, 6, 11, '4aa97f', NULL),
+(158, 'Lazer', 0, 7, 28, '4aa97f', NULL),
+(159, 'Roupas e Acessórios', 0, 8, 33, '4aa97f', NULL),
+(160, 'Água/Luz/Internet', 0, 9, 39, '4aa97f', NULL),
+(161, 'Despesas Diversas', 0, 10, 36, '4aa97f', NULL),
+(162, '*Ajuste*', 0, 2, 36, '4aa97f', NULL),
+(163, 'Salário', 1, 11, 38, '4aa97f', NULL),
+(164, 'Rendimentos', 1, 12, 37, '4aa97f', NULL),
+(165, 'Presentes', 1, 13, 26, '4aa97f', NULL),
+(166, 'Vales', 1, 14, 31, '4aa97f', NULL),
+(167, '*Ajuste*', 1, 3, 37, '4aa97f', NULL),
+(168, 'Outras receitas ', 1, 19, 37, '6d8b97', 0);
 
 -- --------------------------------------------------------
 
@@ -195,7 +230,7 @@ INSERT INTO `despesa` (`id`, `descricao`, `valor`, `status`, `casal`, `usuario`,
 (59, 'Teste', 40.93, 0, '6d8b97', 158, 17, 3, 2024, 113, 0, NULL, 39),
 (60, 'Ajuste de Saldo', 50, 1, '6d8b97', 158, 26, 3, 2024, 141, 0, NULL, 36),
 (61, 'Ajuste de Saldo', 50, 1, '6d8b97', 158, 27, 3, 2024, 141, 0, NULL, 44),
-(62, 'Remédio', 8987, 0, '6d8b97', 158, 13, 4, 2024, 113, 0, NULL, 36),
+(62, 'Remédio', 0, 0, '6d8b97', 158, 16, 4, 2024, 113, 0, NULL, 36),
 (63, 'Aluguel', 500, 0, '6d8b97', 158, 17, 4, 2024, 111, 1, NULL, 39),
 (64, 'Ajuste de Saldo', 4000, 1, '6d8b97', 158, 17, 4, 2024, 141, 1, NULL, 37),
 (65, 'Ajuste de Saldo', 1799390, 1, '6d8b97', 158, 17, 4, 2024, 141, 1, NULL, 37),
@@ -205,9 +240,9 @@ INSERT INTO `despesa` (`id`, `descricao`, `valor`, `status`, `casal`, `usuario`,
 (69, 'Ajuste de Saldo', 3996.04, 1, '6d8b97', 158, 17, 4, 2024, 141, 0, NULL, 44),
 (70, 'Ajuste de Saldo', 4998.33, 1, '6d8b97', 158, 17, 4, 2024, 141, 0, NULL, 44),
 (71, 'Ajuste de Saldo', 7175.11, 1, '6d8b97', 158, 17, 4, 2024, 141, 0, NULL, 51),
-(72, 'Ajuste de Saldo', 0.219992, 1, '6d8b97', 158, 17, 4, 2024, 141, 1, NULL, 39),
 (73, 'Ajuste de Saldo', 7476.86, 1, '6d8b97', 158, 17, 4, 2024, 141, 1, NULL, 37),
-(74, 'Ajuste de Saldo', 4523.14, 1, '6d8b97', 158, 18, 4, 2024, 141, 1, NULL, 37);
+(74, 'Ajuste de Saldo', 4523.14, 1, '6d8b97', 158, 18, 4, 2024, 141, 1, NULL, 37),
+(75, 'Ajuste de Saldo', 4.00998, 1, '6d8b97', 158, 20, 4, 2024, 141, 0, NULL, 36);
 
 -- --------------------------------------------------------
 
@@ -304,14 +339,10 @@ CREATE TABLE `objetivo` (
 
 INSERT INTO `objetivo` (`id`, `descricao`, `valor_final`, `valor_inicial`, `status`, `prazo`, `casal`, `cor`, `icone`) VALUES
 (4, 'Casar', 30000, 7000, 0, '2024-11-30', '6d8b97', 2, 11),
-(5, 'Comprar Carro', 25000, 7000, 0, '2025-06-30', '6d8b97', 4, 16),
 (6, 'Comprar Casa', 150000, 50000, 0, '2025-08-01', '6d8b97', 5, 27),
-(7, 'Comprar celular', 1500, 1000, 0, '2024-05-29', '6d8b97', 16, 39),
-(8, 'Comprar notebook', 4000, 2600, 0, '2024-05-27', '6d8b97', 6, 20),
-(9, 'Arrumar alguma coisa', 500, 450, 0, '2024-05-27', '6d8b97', 16, 36),
-(10, 'Algo mais pra frente', 15000, 10000, 0, '2026-12-04', '6d8b97', 17, 35),
-(12, 'Teste curto', 500, 450, 0, '2024-05-14', '6d8b97', 9, 10),
-(13, 'Teste Longo', 5800, 3000, 0, '2026-01-31', '6d8b97', 2, 21);
+(8, 'Comprar notebook', 4000, 2600, 1, '2024-05-27', '6d8b97', 6, 20),
+(16, 'Teste pra amanhã ', 50, 2, 0, '2024-05-24', '6d8b97', 21, 35),
+(17, 'Objetivo qualquer ', 500, 8, 1, '0000-00-00', '6d8b97', 9, 30);
 
 -- --------------------------------------------------------
 
@@ -359,7 +390,8 @@ INSERT INTO `receita` (`id`, `descricao`, `valor`, `categoria`, `usuario`, `casa
 (120, 'Ajuste de Saldo', 0.00999832, 142, 158, '6d8b97', 1, 0, 17, 4, 2024, 36),
 (121, 'Ajuste de Saldo', 0.89, 142, 158, '6d8b97', 1, 0, 17, 4, 2024, 52),
 (122, 'Ajuste de Saldo', 15029.1, 142, 158, '6d8b97', 1, 0, 17, 4, 2024, 52),
-(123, 'Salário ', 2500, 119, 158, '6d8b97', 0, 0, 18, 4, 2024, 51);
+(123, 'Salário ', 2500, 119, 158, '6d8b97', 0, 0, 18, 4, 2024, 51),
+(125, '', 1258.7, 122, 158, '6d8b97', 0, 0, 0, 0, 0, 51);
 
 -- --------------------------------------------------------
 
@@ -373,6 +405,13 @@ CREATE TABLE `senha_temp` (
   `token` varchar(10) NOT NULL,
   `validade` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `senha_temp`
+--
+
+INSERT INTO `senha_temp` (`id`, `id_usuario`, `token`, `validade`) VALUES
+(29, 158, '62e6', '2024-05-20 22:43:27');
 
 -- --------------------------------------------------------
 
@@ -415,7 +454,23 @@ INSERT INTO `transferencias` (`id`, `descricao`, `valor`, `casal`, `usuario`, `d
 (92, 'Transferência saída', 6000, '6d8b97', 158, 14, 4, 2024, 0, 36, 39, 93),
 (93, 'Transferência entrada', 6000, '6d8b97', 158, 14, 4, 2024, 1, 39, 36, 92),
 (94, 'Transferência saída', 1000, '6d8b97', 158, 14, 4, 2024, 0, 44, 36, 95),
-(95, 'Transferência entrada', 1000, '6d8b97', 158, 14, 4, 2024, 1, 36, 44, 94);
+(95, 'Transferência entrada', 1000, '6d8b97', 158, 14, 4, 2024, 1, 36, 44, 94),
+(96, 'Transferência saída', 50, '6d8b97', 158, 20, 4, 2024, 0, 36, 44, 97),
+(97, 'Transferência entrada', 50, '6d8b97', 158, 20, 4, 2024, 1, 44, 36, 96),
+(98, 'Transferência saída', 50, '6d8b97', 158, 20, 4, 2024, 0, 44, 36, 99),
+(99, 'Transferência entrada', 50, '6d8b97', 158, 20, 4, 2024, 1, 36, 44, 98),
+(100, 'Transferência saída', 50, '6d8b97', 158, 20, 4, 2024, 0, 36, 44, 101),
+(101, 'Transferência entrada', 50, '6d8b97', 158, 20, 4, 2024, 1, 44, 36, 100),
+(102, 'Transferência saída', 50, '6d8b97', 158, 20, 4, 2024, 0, 44, 36, 103),
+(103, 'Transferência entrada', 50, '6d8b97', 158, 20, 4, 2024, 1, 36, 44, 102),
+(104, 'Transferência saída', 50, '6d8b97', 158, 20, 4, 2024, 0, 36, 44, 105),
+(105, 'Transferência entrada', 50, '6d8b97', 158, 20, 4, 2024, 1, 44, 36, 104),
+(106, 'Transferência saída', 50, '6d8b97', 158, 21, 4, 2024, 0, 44, 36, 107),
+(107, 'Transferência entrada', 50, '6d8b97', 158, 21, 4, 2024, 1, 36, 44, 106),
+(108, 'Transferência saída', 50, '6d8b97', 158, 21, 4, 2024, 0, 36, 44, 109),
+(109, 'Transferência entrada', 50, '6d8b97', 158, 21, 4, 2024, 1, 44, 36, 108),
+(110, 'Transferência saída', 10, '6d8b97', 158, 21, 4, 2024, 0, 44, 36, 111),
+(111, 'Transferência entrada', 10, '6d8b97', 158, 21, 4, 2024, 1, 36, 44, 110);
 
 -- --------------------------------------------------------
 
@@ -439,8 +494,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `casal`, `email_parceiro`, `dt_criacao`, `ultimo_acesso`) VALUES
-(158, 'Admin', 'tester@teste.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '6d8b97', 'parceiro@qualquer.com', '2024-03-21 19:43:33', '2024-05-18 01:31:27'),
-(159, 'Parceiro', 'parceiro@qualquer.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '6d8b97', '', '2024-03-21 19:48:08', '2024-04-26 20:42:46');
+(158, 'Admin', 'tester@teste.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '6d8b97', 'parceiro@qualquer.com', '2024-03-21 19:43:33', '2024-05-24 02:09:06'),
+(159, 'Parceiro', 'parceiro@qualquer.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '6d8b97', '', '2024-03-21 19:48:08', '2024-05-21 00:52:21'),
+(180, 'Gideone Lacerda Costa', 'gideonilacerda@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '4aa97f', 'thamilyfelizardo532@gmail.com', '2024-05-20 23:08:30', '0000-00-00 00:00:00');
 
 --
 -- Índices para tabelas despejadas
@@ -450,7 +506,9 @@ INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `casal`, `email_parceiro`
 -- Índices de tabela `aporte_objetivo`
 --
 ALTER TABLE `aporte_objetivo`
-  ADD KEY `fk_aporte_objetivo` (`objetivo`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_aporte_objetivo` (`objetivo`),
+  ADD KEY `fk_aporte_casal` (`casal`);
 
 --
 -- Índices de tabela `banco`
@@ -556,22 +614,28 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de tabela `aporte_objetivo`
+--
+ALTER TABLE `aporte_objetivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT de tabela `banco`
 --
 ALTER TABLE `banco`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de tabela `casal`
 --
 ALTER TABLE `casal`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de tabela `categoria_tr`
 --
 ALTER TABLE `categoria_tr`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=169;
 
 --
 -- AUTO_INCREMENT de tabela `cor`
@@ -583,7 +647,7 @@ ALTER TABLE `cor`
 -- AUTO_INCREMENT de tabela `despesa`
 --
 ALTER TABLE `despesa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT de tabela `icones`
@@ -601,31 +665,31 @@ ALTER TABLE `log`
 -- AUTO_INCREMENT de tabela `objetivo`
 --
 ALTER TABLE `objetivo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `receita`
 --
 ALTER TABLE `receita`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
 
 --
 -- AUTO_INCREMENT de tabela `senha_temp`
 --
 ALTER TABLE `senha_temp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `transferencias`
 --
 ALTER TABLE `transferencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=177;
+  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
 
 --
 -- Restrições para tabelas despejadas
@@ -635,7 +699,7 @@ ALTER TABLE `usuario`
 -- Restrições para tabelas `aporte_objetivo`
 --
 ALTER TABLE `aporte_objetivo`
-  ADD CONSTRAINT `fk_aporte_objetivo` FOREIGN KEY (`objetivo`) REFERENCES `objetivo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_aporte_casal` FOREIGN KEY (`casal`) REFERENCES `casal` (`cod_casal`);
 
 --
 -- Restrições para tabelas `banco`
